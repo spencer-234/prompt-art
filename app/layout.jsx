be5fa0d031eceb834d36163db99ab2e5c3a7b4fd
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-05-06 10:48:13 -0400

    edit readme and meta tags

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import Provider from "@components/Provider/Provider";
 export const metadata = {
   title: "Prompt Art",
   desc: "Share your AI generated images and their prompts!",
+  author: "Spencer Evans",
 };
 
 const RootLayout = ({ children }) => {
